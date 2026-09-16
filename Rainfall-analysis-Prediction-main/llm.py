8073af48fbb0f38802785bc30c92317cdbb30131
soumo2006 <it2024029@rcciit.org.in>
2026-09-16 20:19:10 +0530

    Save local changes before pull

--- Rainfall-analysis-Prediction-main/llm.py
+++ Rainfall-analysis-Prediction-main/llm.py
@@ -70,7 +70,7 @@ Important:
 
 
     response=client.chat.completions.create(
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-120b",
     messages=[
         {
             "role": "system",
